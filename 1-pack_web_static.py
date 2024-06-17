63474eedf410d76b8d9f5384a987f7b9e04f83cc
Abdessattar <ghabi.1602@gmail.com>
2024-06-17 07:24:27 -0700

    Update repo

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+from fabric.api import local
+from datetime import datetime
+from op.path import isdir
+
+def do_pack():
+    try:
+        date = datetime.now().strftime('%Y%m%d%H%M%S')
+        if isdir(versions) is False:
+            local('mkdir versions')
+
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_server".format(file_name))
+        return file_name
+    except:
+        return None
